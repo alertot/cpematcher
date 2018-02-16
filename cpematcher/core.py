502feca2facfa42308971183b148b39753151ef7
Claudio Salazar <csalazar@spect.cl>
2018-02-16 19:09:34 -0300

    Add version border constraints

--- cpematcher/core.py
+++ cpematcher/core.py
@@ -1,5 +1,7 @@
 import fnmatch
 
+from .version import Version
+
 OR_OPERATOR = 'OR'
 AND_OPERATOR = 'AND'
 
@@ -11,7 +13,15 @@ class CPE:
         'language', 'sw_edition', 'target_sw', 'target_hw', 'other',
     ]
 
-    def __init__(self, cpe_str, vulnerable=True):
+    def __init__(
+            self,
+            cpe_str,
+            vulnerable=True,
+            version_start_including=None,
+            version_start_excluding=None,
+            version_end_including=None,
+            version_end_excluding=None,
+        ):
         ''' Create CPE object with information about affected software.
 
         Usually CPE is used to find out if a version is vulnerable,
@@ -32,9 +42,16 @@ class CPE:
             setattr(self, f, values.pop(0))
 
         self.is_vulnerable = vulnerable
-
+        self.version_start_including = Version(version_start_including)
+        self.version_start_excluding = Version(version_start_excluding)
+        self.version_end_including = Version(version_end_including)
+        self.version_end_excluding = Version(version_end_excluding)
 
     def matches(self, another_cpe):
+        """ Verify if `another_cpe` matches, first through field comparison and
+        then using the border constraints.
+
+        """
         for f in self.fields:
             value = getattr(self, f)
             another_value = getattr(another_cpe, f)
@@ -42,10 +59,57 @@ class CPE:
             if not fnmatch.fnmatch(another_value, value):
                 return False
 
+        version = Version(another_cpe.version)
+
+        # Do verifications on start version
+        if self.version_start_including and version < self.version_start_including:
+            return False
+
+        if self.version_start_excluding and version <= self.version_start_excluding:
+            return False
+
+        if self.version_end_including and version > self.version_end_including:
+            return False
+
+        if self.version_end_excluding and version >= self.version_end_excluding:
+            return False
+
         return True
 
 
 class CPEOperation:
+    ''' Handle operations defined on CPE sets.
+
+    Support for:
+        - OR operations
+
+    '''
+    VERSION_MAP = {
+        'vsi': [
+            'versionStartIncluding',
+            'version_start_including',
+        ],
+        'vse': [
+            'versionStartExcluding',
+            'version_start_excluding',
+        ],
+        'vei': [
+            'versioEndIncluding',
+            'version_end_including',
+        ],
+        'vee': [
+            'versioEndExcluding',
+            'version_end_excluding',
+        ],
+    }
+
+    def _get_value(self, cpe_dict, key):
+        for k in self.VERSION_MAP[key]:
+            if k in cpe_dict.keys():
+                return cpe_dict[k]
+
+        return None
+
     def __init__(self, operation_dict):
         self.cpes = set()
 
@@ -53,7 +117,15 @@ class CPEOperation:
 
         if operator == OR_OPERATOR:
             for cpe_dict in operation_dict['cpe']:
-                c = CPE(cpe_dict['cpe23Uri'], cpe_dict['vulnerable'])
+                c = CPE(
+                    cpe_dict['cpe23Uri'],
+                    cpe_dict['vulnerable'],
+                    version_start_including=self._get_value(cpe_dict, 'vsi'),
+                    version_start_excluding=self._get_value(cpe_dict, 'vse'),
+                    version_end_including=self._get_value(cpe_dict, 'vei'),
+                    version_end_excluding=self._get_value(cpe_dict, 'vee'),
+                )
+
                 self.cpes.add(c)
 
     def matches(self, another_cpe):
--- tests/test_core.py
+++ tests/test_core.py
@@ -4,6 +4,8 @@ from cpematcher.core import CPE, CPEOperation
 
 
 class TestCPE:
+    template = 'cpe:2.3:a:apache:activemq:%s:*:*:*:*:*:*:*'
+
     def test_init(self):
         c = CPE('cpe:2.3:a:apache:activemq:4.0.1:*:*:*:*:*:*:*')
         assert c.vendor == 'apache'
@@ -38,6 +40,34 @@ class TestCPE:
         version_cpe = CPE('cpe:2.3:a:apache:activemq:4.1.1:*:*:*:*:*:*:*')
 
         assert version_cpe.matches(version_cpe)
+
+    def test_matches_with_version_start_including(self):
+        branch_cpe = CPE(self.template % '4.1.*', version_start_including='4.1.3')
+
+        assert not branch_cpe.matches(CPE(self.template % '4.1.2'))
+        assert branch_cpe.matches(CPE(self.template % '4.1.3'))
+        assert branch_cpe.matches(CPE(self.template % '4.1.4'))
+
+    def test_matches_with_version_start_excluding(self):
+        branch_cpe = CPE(self.template % '4.1.*', version_start_excluding='4.1.3')
+
+        assert not branch_cpe.matches(CPE(self.template % '4.1.2'))
+        assert not branch_cpe.matches(CPE(self.template % '4.1.3'))
+        assert branch_cpe.matches(CPE(self.template % '4.1.4'))
+
+    def test_matches_with_version_end_including(self):
+        branch_cpe = CPE(self.template % '4.1.*', version_end_including='4.1.3')
+
+        assert branch_cpe.matches(CPE(self.template % '4.1.2'))
+        assert branch_cpe.matches(CPE(self.template % '4.1.3'))
+        assert not branch_cpe.matches(CPE(self.template % '4.1.4'))
+
+    def test_matches_with_version_end_excluding(self):
+        branch_cpe = CPE(self.template % '4.1.*', version_end_excluding='4.1.3')
+
+        assert branch_cpe.matches(CPE(self.template % '4.1.2'))
+        assert not branch_cpe.matches(CPE(self.template % '4.1.3'))
+        assert not branch_cpe.matches(CPE(self.template % '4.1.4'))
 
 
 class TestCPEOperation:
